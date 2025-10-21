75984285946d793697c72e9bc9ad4bf9110a59fc
Jan394123 <jan.schwarzl@tfs-haslach.at>
2025-10-21 11:44:48 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const server = express();
+const PORT = 3000;
+
+// Static files (HTML, CSS, JS, images)
+server.use(express.static(__dirname));
+server.use('/content', express.static(path.join(__dirname, 'content')));
+server.use('/img', express.static(path.join(__dirname, 'img')));
+
+// Fallback to index.html for root
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(PORT, () => {});
